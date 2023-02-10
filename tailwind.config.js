d455b8276b58c11aa892594ae9170e94d46036a9
dileepa dev <dileepesandaruwan@gmail.com>
2023-02-10 16:04:03 +0530

    chandima personal web editing home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'lon': ['Londrina Outline', 'cursive'],
+       
+      },
+    },
   },
   plugins: [],
 }
